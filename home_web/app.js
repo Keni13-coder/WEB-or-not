7af417d18bbc188ee39733eb56f3546d10c64a36
Vlad <vlad.moldovanov.2014@mail.ru>
2023-04-11 09:16:02 +0300

    finished the first part

--- home_web/app.js
+++ home_web/app.js
@@ -1,21 +1,27 @@
-
+window.addEventListener('scroll', (d) => {
+	document.getElementById("myId").style.setProperty('--scrollTop', `${this.scrollY}px`)})
 let btns = document.querySelector(".btn");
 
 btns.onclick = (e) =>{  
     var namee  = document.getElementById("nam").value;
     var value = document.getElementById('em').value;
         if (value === ''|| (namee === '')) {
-            alert('Oops, there are still empty fields left')}
+            alert('Ммм...');
+            alert('Странник ты что-то забыл.');
+            alert('Хммм...');
+            alert('Что же это?');
+        }
     
         else{
             var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             var valid = re.test(value);
             if (valid){
                 document.getElementById("myId").disabled = false;
-
-            alert(`${namee} Тебе открылся путь в Земли Рассвета`)}
+                var bt = document.getElementById("myId");
+                bt.setAttribute("style","opacity: 1 ; transition: opacity 4s; transform: translate3d(0,calc(var(--scrollTop) / 5.7),600px);font-size:calc(var(--index)*5);" );
+                alert(`${namee} Тебе открылся путь в Земли Рассвета`)}
             else{
-                alert("пидор")
+                alert(`Прилядись ${namee} ты что-то упускаешь.`)
             }}    
            
 
